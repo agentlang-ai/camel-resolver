37f94571042cd2b0423f47f5c812b81c018ebdf0
Vijay Mathew <vijay@fractl.io>
2024-05-15 12:08:35 +0530

    first commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject com.github.fractl-io/camel-resolver "0.0.1"
+  :dependencies [[com.github.fractl-io/fractl "0.5.1"]
+                 [org.apache.camel/camel-core "4.6.0"]]
+  :aot :all)
