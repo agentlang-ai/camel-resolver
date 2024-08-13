38da780ee086d6cefd597124a44146a52484324a
Muazzam Kazmi <118562685+muazzam0x48@users.noreply.github.com>
2024-08-13 21:43:56 +0500

    bump fractl version to 0.5.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject com.github.fractl-io/camel-resolver "0.0.1"
-  :dependencies [[com.github.fractl-io/fractl "0.5.3"]
+  :dependencies [[com.github.fractl-io/fractl "0.5.4"]
                  [org.apache.camel/camel-core "4.6.0"]]
   :aot :all)
