d806c72983091c187b6ec1e29596735fd3ac36c9
Muazzam Kazmi <muazzam@fractl.io>
2025-03-14 22:31:16 +0500

    bump agentlang version to 0.6.2

--- example/camel/project.clj
+++ example/camel/project.clj
@@ -1,5 +1,5 @@
 (defproject fractl.example/camel "0.0.1"
-  :dependencies [[com.github.agentlang-ai/agentlang "0.6.1-alpha3"]
+  :dependencies [[com.github.agentlang-ai/agentlang "0.6.2"]
                  [com.github.fractl-io/camel-resolver "0.0.1"]
                  [org.apache.camel/camel-main "4.6.0"]
                  [org.apache.camel/camel-jackson "4.6.0"]
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject com.github.agentlang-ai/camel-resolver "0.0.1"
-  :dependencies [[com.github.agentlang-ai/agentlang "0.6.1-alpha3"]
+  :dependencies [[com.github.agentlang-ai/agentlang "0.6.2"]
                  [org.apache.camel/camel-core "4.6.0"]]
   :aot :all)
